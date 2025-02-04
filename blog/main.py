dcef984d4c5da98697a544082d430d1de6e5c66a
EmanAmir13 <eman.amir@flicken.io>
2025-02-04 17:07:22 +0500

    add the update and delete blog code

--- blog/main.py
+++ blog/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI, Depends, status, Response, HTTPException
+from sqlalchemy.orm import Session
 
 from blog.schemas import Blog
 from . import models
 from .database import engine, get_db
-from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -24,11 +24,31 @@ def get_blogs(db: Session = Depends(get_db)):
     return db.query(models.Blog).all()
 
 
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(id: int, db: Session = Depends((get_db))):
+    blog = db.query(models.Blog).filter(id == models.Blog.id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'The blog with id {id} not found')
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
+@app.put('/blog/{id}', status_code=200)
+def update_blog(id: int, request: Blog, db: Session = Depends(get_db)):
+    blog = (db.query(models.Blog).filter(id == models.Blog.id))
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'The blog with id {id} not found')
+    blog.update(request.dict())
+    db.commit()
+    return 'updated'
+
+
 @app.get('/blog/{id}', status_code=200)
 def get_blog_by_id(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(id == models.Blog.id).first()
     if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'The blog for id {id} not available')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'The blog for id {id} not found')
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return f'The blog for id {id} not available'
     return blog
