ba1586751eeb443c2b6483f61628f40fdfd67d55
EmanAmir13 <eman.amir@flicken.io>
2025-02-10 10:25:15 +0500

    add show blog schema

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,28 +1,37 @@
+from typing import List
+
 from pydantic import BaseModel
 
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title: str
     body: str
 
 
-class Show(BaseModel):
-    title: str
-    body: str
-
+class Blog(BlogBase):
     class Config():
         orm_mode = True
 
 
 class User(BaseModel):
-    email: str
     username: str
+    email: str
     password: str
 
 
 class ShowUser(BaseModel):
-    email: str
     username: str
+    email: str
+    blogs: List[Blog] = []
+
+    class Config():
+        orm_mode = True
+
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
 
     class Config():
         orm_mode = True
